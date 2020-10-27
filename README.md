bd757ad103dd070a91cd73c3bbdcef7bd6a29bbd
Dblply21 <62082121+Dblply21@users.noreply.github.com>
2020-10-27 01:33:18 -0500

    add files

---
+++ README.md
@@ -0,0 +1,34 @@
+Note Taker
+screencapture-gentle-stream-25122-herokuapp-2020-07-31-19_31_22
+
+Description
+This application allows users to create and save notes to a Heroku server in order to retrieve them later.
+
+Table of Contents
+Installation
+Usage
+License
+Contributing
+Tests
+Questions
+Installation
+To install, clone the source code and run 'npm i' in your command line.
+
+Usage
+The application is used in a chrome browser with the link provided.
+
+https://gentle-stream-25122.herokuapp.com/
+
+License
+N/A
+
+Contributing
+N/A
+
+Tests
+N/A
+
+Questions
+Contact: jesusnboone@gmail.com
+
+Github Username: https://github.com/jesusnboone
---
+++ server.js
@@ -0,0 +1,53 @@
+// Dependencies
+// =============================================================
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+const notes = require('./db/db.json')
+
+
+// Routes
+// =============================================================
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+  return res.json(notes);
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  newNote.title = newNote.title
+  newNote.text = newNote.text
+  newNote.id = newNote.title
+
+  console.log(newNote);
+
+  notes.push(newNote);
+
+  res.json(newNote);
+});
+
+// Listener
+// =============================================================
+app.listen(PORT, () => {
+  console.log(`App listening on PORT ${PORT}`);
+});
